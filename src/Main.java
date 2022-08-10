d4c9716bdff4e8ac6e7ae64f79d6d44bdb96d951
ma-neme <maira.dombroski@gmail.com>
2022-08-09 21:44:18 -0300

    mentoria + curso

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();  //classe é como se fosse a planta de uma casa, objeto como se fosse a casa contruída
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso JS");
+        curso2.setDescricao("Descrição curso JavaScript");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria Java");
+        mentoria1.setDescricao("Descrição mentoria Java");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(mentoria1);
+        //System.out.println(curso2);
+
+    }
+}
